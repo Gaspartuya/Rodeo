0ea226bb858a53f29de2a545ed85c8c8c9da2ef3
= <=>
2022-10-18 16:52:31 +0200

    actualizacion

--- assets/js/plantilla.js
+++ assets/js/plantilla.js
@@ -1,41 +1,6 @@
 let Animales = [];
 const cart = [];
-
-
-    const loadEnvents = () =>
-        {   
-            let buttons = document.querySelectorAll('boton');
-            for (const button of buttons)
-            {
-                button.addEventListener('click', ()=>{
-                    let found = cart.find(element => element.id == button.id);
-                    if (found)
-                {
-                    
-                    found.quantity++;
-                }
-                else 
-                {
-                    let Animal = Animales.find(element => element.id == button.id)
-             
-
-                    if (Animal)
-                    {
-                        let newProduct = {
-                            Tipo: Animal.Tipo,
-                            Numero: Animal.Numero,
-                            Raza: Animal.Raza,
-                            edad: Animal.edad,
-                            TiempoDeEmbarazo: Animal.TiempoDeEmbarazo
-
-                        }
-                        cart.push(newProduct)
-                    }
-            }
-            updateCart(cart);
-        })
-    }
-}
+const filtroProducto = document.getElementById("filtroProducto")
 
 
 const mostrarError = () => {
@@ -56,19 +21,18 @@ const mostrarVaca = (Animales) => {
                         <td>${Raza}</td>
                         <td>${edad}</td>
                         <td>${TiempoDeEmbarazo}</td>
-                        <td><button id="boton">+</button></td>
+                        <td><button id='boton${Numero}'>+</button></td>
                     </tr>
                 </tbody>`
     }
     
 const contenedor = document.querySelector(".vaca")
-const URL = "../js/vacas.json"
 
 let contenidoHTML = ""
 
 const cargarContenido = async () => {
     try {
-        const response = await fetch(URL)
+        const response = await fetch ('../js/vacas.json')
         const data = await response.json()
             Animales = data
             Animales.forEach(etiqueta => contenidoHTML += mostrarVaca(etiqueta))
@@ -81,3 +45,45 @@ const cargarContenido = async () => {
 }
 
 cargarContenido(Animales) 
+
+const inputFiltrar = document.querySelector("input")
+
+function filtrarProductos() { 
+    inputFiltrar.value = inputFiltrar.value.trim().toUpperCase()
+    if (inputFiltrar.value !== "") {
+        const resultado = Animales.filter(Animal => Animal.Numero.includes(inputFiltrar.value))
+              if (resultado.length === 0) {
+                console.clear()
+                console.warn("No se encontraron productos.")
+                cargarContenido(Animales) 
+              } else {
+                cargarContenido(resultado)
+              }
+    } else {
+        cargarContenido(Animales) 
+    }
+}
+
+inputFiltrar.addEventListener("input", filtrarProductos)
+
+function eventoEnBotones() {
+    Animales.forEach(prod => {
+        const btn = document.querySelector('#boton${Numero}')
+              btn.addEventListener("click", ()=> agregarAlCarrito(`${Numero}`))
+    })
+}
+
+eventoEnBotones()
+
+function agregarAlCarrito(Numero) {
+    const Animal = productos.find(prod => prod.Numero == Numero)
+          carrito.push(Animal)
+          localStorage.setItem("carrito", JSON.stringify(carrito))
+}
+
+function recuperarCarrito() {
+    if (localStorage.getItem("carrito")) {
+        carrito = JSON.parse(localStorage.getItem("carrito"))
+    }
+}
+recuperarCarrito()
